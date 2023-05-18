2e346846015f67c6c55226bf4a1dc89d1c61c61c
Thenuri <cb010529@students.apiit.lk>
2023-05-18 08:36:21 +0530

    middleware

---
+++ app/Http/Controllers/Approval.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class Approval extends Controller
+{
+
+    public function switchStatus(Request $request, User $user)
+    {
+        $request->offsetSet('status', $request->status == 1 ? 1 : 0);
+
+
+        $user->update([
+            'status' => $request->status,
+        ]);
+
+    
+
+       return response()->redirectTo('/dashboard/employee');
+    }
+}
+
+
