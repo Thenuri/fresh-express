9c8b8cd4097da098274ad0a9ee272062e6432403
Thenuri <cb010529@students.apiit.lk>
2023-05-19 22:27:10 +0530

    middleware

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Approval;
 use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\EmployeeController;
+use Illuminate\Routing\Controllers\Middleware;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 /*
@@ -19,31 +20,31 @@ use App\Http\Controllers\HomeController;
 // Route::get('dashboard', function () {
 //     return view('welcome');
 // });
+// Route::get('/admin/approval', [AdminController::class, 'approval'])->name('admin.approval');
+
+
 Route::get('/', [HomeController::class,'checkRoleid']);
 
-Route::resource('dashboard/customer', CustomerController::class)->name('index', 'customer');
+Route::post('/admin/change-status/{user}', [Approval::class, 'switchStatus'])
+->name('admin.approve')->middleware('adminOrEmployee');
 
-Route::resource('dashboard/employee', EmployeeController::class)->name('index', 'employee');
-
-//Route::get('/admin/approval', [AdminController::class, 'approval'])->name('admin.approval');
-Route::resource('dashboard/approval', Approval::class)->name('index', 'approval');
-
-Route::post('/admin/change-status/{user}', [Approval::class, 'switchStatus'])->name('admin.approve');
-Route::view('dashboard/orders', 'dashboard.orders')->name('orders');
-Route::view('dashboard/products', 'dashboard.products')->name('products');
-
-
+Route::middleware(['auth'])->group(function () {
+    Route::resource('dashboard/customer', CustomerController::class)->name('index', 'customer');
+    Route::resource('dashboard/employee', EmployeeController::class)->name('index', 'employee');
+    Route::resource('dashboard/approval', Approval::class)->name('index', 'approval');
+    Route::view('dashboard/orders', 'dashboard.orders')->name('orders');
+    Route::view('dashboard/products', 'dashboard.products')->name('products');
+});
 
 Route::get('/admin', function () {
     return view('dashboard.index');
 })->name('admin.dashboard')
-->middleware(['adminOrEmployee','userApproved']);
-
+->middleware(['adminOrEmployee','auth']);
 
 Route::get('/employee', function () {
     return view('dashboard.index');
 })->name('employee.dashboard')
-->middleware([ 'userApproved']);
+->middleware(['adminOrEmployee','userApproved']);
 
 Route::get('/driver', function () {
     return view('driverdash');
